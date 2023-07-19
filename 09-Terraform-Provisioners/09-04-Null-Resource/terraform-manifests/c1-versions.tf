c389db60e8913a0fdea90557b7e5fedcd597a57f
alldevice2018 <assiattot@gmail.com>
2023-07-18 21:07:12 -0400

    new update

--- 09-Terraform-Provisioners/09-04-Null-Resource/terraform-manifests/c1-versions.tf
+++ 09-Terraform-Provisioners/09-04-Null-Resource/terraform-manifests/c1-versions.tf
@@ -1,19 +1,19 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.4" 
+  required_version = ">= 1.2"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "~> 3.0.0"
     }
     time = {
-      source = "hashicorp/time"
+      source  = "hashicorp/time"
       version = "~> 0.6.0"
-    }      
+    }
   }
 }
 
--- 10-Terraform-Modules/10-01-Terraform-Modules-Basics/terraform-manifests/c5-outputs.tf
+++ 10-Terraform-Modules/10-01-Terraform-Modules-Basics/terraform-manifests/c5-outputs.tf
@@ -2,15 +2,20 @@
 
 output "ec2_instance_public_ip" {
   description = "Public IP Addressess of EC2 Instances"
-  value = module.ec2_cluster.*.public_ip
+  value       = module.ec2_cluster.*.public_ip
 }
 
 output "ec2_instance_public_dns" {
   description = "Public DNS for EC2 Instances"
-  value = module.ec2_cluster.*.public_dns
+  value       = module.ec2_cluster.*.public_dns
+}
+
+output "ec2_instance_primary_network_interface_id" {
+  description = "Id of the primary network interface"
+  value       = module.ec2_cluster.*.primary_network_interface_id
 }
 
 output "ec2_instance_private_ip" {
   description = "Private IP Addresses for EC2 Instances"
-  value = module.ec2_cluster.*.private_ip
+  value       = module.ec2_cluster.*.private_ip
 }
